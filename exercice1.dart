2f3750a93b1cb577ed1da08c00b1612d4e0f77d7
zakariadjellouli <djellouli.zakaria.zd@gmail.com>
2025-11-14 20:01:38 -0500

    exercice 1 : commit question 5

--- exercice1.dart
+++ exercice1.dart
@@ -4,4 +4,11 @@ class Couleur {
   final int b;
 
   Couleur(this.r, this.g, this.b);
+  Couleur.rouge() : r = 255, g = 0, b = 0;
+  Couleur.vert() : r = 0, g = 255, b = 0;
+  Couleur.bleu() : r = 0, g = 0, b = 255;
+
+  void afficherCouleur() {
+    print("(R : $r, V : $g, B : $b)");
+  }
 }
