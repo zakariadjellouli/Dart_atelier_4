392729400c6514e3b90627bac47844ad0b276dba
zakariadjellouli <djellouli.zakaria.zd@gmail.com>
2025-11-14 20:15:02 -0500

    exercice 2 : commit question 4

--- exercice2.dart
+++ exercice2.dart
@@ -3,4 +3,12 @@ class Point {
   double _y;
 
   Point(this._x, this._y);
+  void move(double dx, double dy) {
+    _x += dx;
+    _y += dy;
+  }
+
+  void display() {
+    print("Point: ($_x, $_y)");
+  }
 }
