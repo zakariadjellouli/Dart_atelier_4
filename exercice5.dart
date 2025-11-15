487bad880a85ee8f9acf5fc722e8f3d88d6ff525
zakariadjellouli <djellouli.zakaria.zd@gmail.com>
2025-11-14 22:13:54 -0500

    exercice 5 : commit question 5

--- exercice5.dart
+++ exercice5.dart
@@ -1,7 +1,18 @@
+import 'dart:math';
+
 abstract class Forme {
   double calculerAire();
 
   void afficherMessage() {
     print("Calcul de l'aire...");
+  }
+}
+
+class Cercle extends Forme {
+  final double rayon;
+  Cercle(this.rayon);
+  @override
+  double calculerAire() {
+    return pi * rayon * rayon;
   }
 }
